3efe7dc97b626d4ca78d6ecfff48085cb3f2aba7
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-02-17 18:21:50 -0500

    Integration with telegram bot.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const morgan = require("morgan");
 const colors = require("colors");
 const helmet = require("helmet");
+const TelegramBot = require("node-telegram-bot-api");
 
-// load environment variables
-dotenv.config();
+const { BOT_API_KEY, HEROKU_APP_URL } = process.env;
 
 const PORT = process.env.PORT || 5000;
+
+const bot = new TelegramBot(BOT_API_KEY);
+bot.setWebHook(`${HEROKU_APP_URL}/bot${BOT_API_KEY}`);
 
 const app = express();
 app.use(helmet());
@@ -21,6 +24,11 @@ app.use("/api/v1/calculate", require("./routes/calculate"));
 
 app.use(require("./middleware/error")); // error handler
 
+app.post(`/bot${BOT_API_KEY}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
 const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
@@ -32,4 +40,8 @@ process.on("unhandledRejection", (err, promise) => {
 
   // close server
   server.close(() => process.exit(1));
+});
+
+bot.on("message", msg => {
+  bot.sendMessage(msg.chat.id, "I am alive!");
 });
