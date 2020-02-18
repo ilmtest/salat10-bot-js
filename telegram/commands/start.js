ec85a17d4f12631ad2c9c740b6f53849014a5779
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-02-17 21:41:59 -0500

    Push up

--- telegram/commands/start.js
+++ telegram/commands/start.js
@@ -1,10 +1,10 @@
 const analytics = require("../../utils/analytics");
 
 const startCommand = bot => {
-  bot.onText(/^\/start$/, msg => {
+  bot.onText(/^\/start$/, message => {
     console.log("Replying to user with start response");
     bot.sendMessage(
-      msg.chat.id,
+      message.chat.id,
       `\`${process.env.npm_package_name} v${process.env.npm_package_version}\` by @ilmtest
   
   Send this bot an address, or attach your location to it and it will reply back with the prayer times for that location.
@@ -15,7 +15,7 @@ const startCommand = bot => {
   Note: Please read the Help before filing any bugs of incorrect timings.
   `,
       {
-        reply_to_message_id: msg.message_id,
+        reply_to_message_id: message.message_id,
         reply_markup: {
           inline_keyboard: [
             [
