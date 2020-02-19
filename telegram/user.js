cd4a5b81d50fd82e268ecfa3161f71206f3afb9d
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-02-19 18:26:10 -0500

    Fix address.

--- telegram/user.js
+++ telegram/user.js
@@ -1,5 +1,5 @@
 const userMiddleware = bot => {
-  ["start", "help", "bug"].forEach(command =>
+  ["address", "start", "help", "bug"].forEach(command =>
     require(`./commands/${command}`)(bot)
   );
 
