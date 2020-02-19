5d27fde23ee49f36d885edf39d53d07a33599301
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-02-19 16:53:24 -0500

    Add bug report reply.

--- telegram/handlers/bugReply.js
+++ telegram/handlers/bugReply.js
@@ -1,47 +1,56 @@
+const analytics = require("../../utils/analytics");
+
 const isReplyDeleted = error =>
   error.code === 400 &&
   error.description === "Bad Request: reply message not found";
 
-const handleBugReply = async (context, next) => {
-  if (!context.state.private) {
-    return next();
-  }
+const bugReply = bot => {
+  bot.on(
+    "text",
+    async ({ reply_to_message, chat, text, date, message_id: replyId }) => {
+      if (chat.id !== process.env.CONTACT_CHAT_ID) {
+        return;
+      }
 
-  const { reply_to_message } = context.update.message;
+      const [chat_id, message_id] = reply_to_message.text
+        .split("\n")
+        .slice(1, 3);
+      const [chatId, messageId] = [chat_id, message_id].map(
+        value => value.split(": ")[1]
+      );
 
-  if (!reply_to_message) {
-    return next();
-  }
+      if (chatId && messageId) {
+        try {
+          await bot.sendMessage(chatId, text, {
+            reply_to_message_id: messageId
+          });
+        } catch (error) {
+          if (isReplyDeleted(error)) {
+            console.warn(
+              "Original message was deleted, not replying.",
+              chatId,
+              messageId
+            );
+            bot.sendMessage(chatId, text);
 
-  const [chat_id, message_id] = reply_to_message.text.split("\n").slice(1, 3);
-  const [chatId, messageId] = [chat_id, message_id].map(
-    value => value.split(": ")[1]
-  );
+            const now = Math.floor(Date.now() / 1000);
 
-  if (chatId && messageId) {
-    try {
-      await context.telegram.sendMessage(chatId, context.message.text, {
-        reply_to_message_id: messageId
-      });
-    } catch (error) {
-      if (isReplyDeleted(error)) {
-        console.warn(
-          "Original message was deleted, not replying.",
-          chatId,
-          messageId
-        );
-        await context.telegram.sendMessage(chatId, context.message.text);
+            analytics.track({
+              userId: chatId,
+              event: "deleted_bug_report",
+              properties: {
+                diff: (now - date).toString()
+              }
+            });
+          }
+        }
+      } else {
+        bot.sendMessage(chat.id, "Invalid message format", {
+          reply_to_message_id: replyId
+        });
       }
     }
-  } else {
-    return next();
-  }
-};
-
-const bugReply = bot => {
-  bot.on("text", message => {
-    console.log("*** message", message);
-  });
+  );
 };
 
 module.exports = bugReply;
