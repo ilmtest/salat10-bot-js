b79999dc0f46c506dec6249b7b208c4d73096db2
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-02-20 00:25:53 -0500

    Help command cleanup.

--- telegram/commands/help.js
+++ telegram/commands/help.js
@@ -2,7 +2,7 @@ const analytics = require("../../utils/analytics");
 const fs = require("fs");
 
 const helpCommand = bot => {
-  bot.onText(/^\/help$/, async message => {
+  bot.onText(/^\/help$/, message => {
     console.log("Replying to user with help command");
     bot.sendMessage(
       message.chat.id,
@@ -11,9 +11,10 @@ const helpCommand = bot => {
 The other angles employed by the various other organizations do not produce the right results of the correct entrance of Fajr that is visible when one observes it with the sight.
 https://ilmtest.tumblr.com/post/172226611121/fajr
 
-Tutorial: https://s5.gifyu.com/images/tutorial.gif
+Also it is important to always know that the origin of the matter is to observe the timings of the prayers using the eyesight just as it was done at the time of the Messenger ﷺ. As a result, prayer time apps such as this should only be used as a supporting guide and not as the primary dependency.
 
-Also it is important to always know that the origin of the matter is to observe the timings of the prayers using the eyesight just as it was done at the time of the Messenger ﷺ. As a result, prayer time apps such as this should only be used as a supporting guide and not as the primary dependency.`,
+Tutorial on how to use the bot:
+https://s5.gifyu.com/images/tutorial.gif`,
       {
         reply_to_message_id: message.message_id
       }
