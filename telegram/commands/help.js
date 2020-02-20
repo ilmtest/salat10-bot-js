f19d0dba0a68fc1ce0cd0c80c84dfb2b36ab33a3
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2020-02-20 00:19:00 -0500

    Send message instead of animation.

--- telegram/commands/help.js
+++ telegram/commands/help.js
@@ -3,21 +3,17 @@ const fs = require("fs");
 
 const helpCommand = bot => {
   bot.onText(/^\/help$/, async message => {
-    //const buffer = fs.readFileSync("res/tutorial.gif");
-
     console.log("Replying to user with help command");
-    bot.sendChatAction(message.chat.id, "upload_video");
-    bot.sendAnimation(
+    bot.sendMessage(
       message.chat.id,
-      "https://s5.gifyu.com/images/tutorial.gif",
-      {
-        caption: `Note that Salat10 uses the nautical twilight to calculate the Fajr and ʿIshāʾ timings instead of the conventional varying angles that are used by the various organizations around the world.
+      `Note that Salat10 uses the nautical twilight to calculate the Fajr and ʿIshāʾ timings instead of the conventional varying angles that are used by the various organizations around the world.
 
 The other angles employed by the various other organizations do not produce the right results of the correct entrance of Fajr that is visible when one observes it with the sight.
 https://ilmtest.tumblr.com/post/172226611121/fajr
 
-Also it is important to always know that the origin of the matter is to observe the timings of the prayers using the eyesight just as it was done at the time of the Messenger ﷺ. As a result, prayer time apps such as this should only be used as a supporting guide and not as the primary dependency.`
-      },
+Tutorial: https://s5.gifyu.com/images/tutorial.gif
+
+Also it is important to always know that the origin of the matter is to observe the timings of the prayers using the eyesight just as it was done at the time of the Messenger ﷺ. As a result, prayer time apps such as this should only be used as a supporting guide and not as the primary dependency.`,
       {
         reply_to_message_id: message.message_id
       }
